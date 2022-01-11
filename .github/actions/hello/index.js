0834989247caac9712da3fe19af6aa56e29871a5
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2022-01-11 00:39:13 +0000

    fix: add more features index.js

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,9 +4,9 @@ const github = require("@actions/github");
 try {
   //throw(new Error("some error message"));
 
-  core.debug('Debug message');
-  core.warning('Warning message');
-  core.error('Error message');
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
 
   const name = core.getInput("who-to-greet");
   core.setSecret(name);
@@ -15,12 +15,11 @@ try {
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
-  core.startGroup('Logging github object');
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github), null, "\t");
   core.endGroup();
 
-  core.exportVariable('HELLO', 'hello');
-
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
